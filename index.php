3624ac5c10a01905883ead43d627ab03c7e98135
crackbrus86 <crackbrus86@gmail.com>
2023-05-07 17:21:52 +0300

    Global: Updated version

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ add_shortcode("Ratings", "ratingsTableApp");
 
 class UPFRatings
 {
-    public static $appVersion = "3.1.0.03182023";
+    public static $appVersion = "3.1.1.05072023";
 
     public function initRating()
     {
